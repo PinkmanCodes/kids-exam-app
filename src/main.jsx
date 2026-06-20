cd884f8ef8c59d06b49e36eb8d2d8830b9c7dba8
V Prasana <v.prasana.external@usu.com>
2026-06-20 21:16:05 +0530

    index head tag update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 import store from "./app/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter  basename="/kids-exam-app">
       <App />
     </BrowserRouter>
   </Provider>,
